39ed80d3af87997134e07f279f1ff863347cd65f
thikimyen-nguyen <nguyenthikimyen25@gmail.com>
2023-11-22 16:02:44 +0100

    build html home page

---
+++ js/auth_API/api.mjs
@@ -0,0 +1,6 @@
+const baseUrl = "https://api.noroff.dev/api/v1";
+const listingsEndpoint = "/auction/listings";
+const allListings = baseUrl + listingsEndpoint;
+
+
+export { allListings }
---
+++ js/index.mjs
@@ -0,0 +1,16 @@
+import { allListings } from "./auth_API/api.mjs";
+
+async function getListings(url) {
+  const response = await fetch(url);
+  const listings = await response.json();
+  if (response.ok) {
+    console.log(listings)
+    return listings;
+    
+  } else {
+    throw new Error("Could not get posts!")
+  }
+}
+getListings(allListings);
+
+
