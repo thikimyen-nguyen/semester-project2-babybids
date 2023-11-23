340a0cdf03900e543dad338d413668ad12ebc37a
thikimyen-nguyen <nguyenthikimyen25@gmail.com>
2023-11-23 23:44:13 +0100

    test action

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -3,16 +3,16 @@ import { searchIcon, showSearchForm } from "../js/components/search.mjs";
 
 searchIcon.addEventListener("click", showSearchForm);
 
-// import { allListings } from "./auth_API/api.mjs";
+import { allListings } from "./auth_API/api.mjs";
 
-// async function getListings(url) {
-//   const response = await fetch(url);
-//   const listings = await response.json();
-//   if (response.ok) {
-//     console.log(listings);
-//     return listings;
-//   } else {
-//     throw new Error("Could not get posts!");
-//   }
-// }
-// getListings(allListings);
+async function getListings(url) {
+  const response = await fetch(url);
+  const listings = await response.json();
+  if (response.ok) {
+    console.log(listings);
+    return listings;
+  } else {
+    throw new Error("Could not get posts!");
+  }
+}
+getListings(allListings);
