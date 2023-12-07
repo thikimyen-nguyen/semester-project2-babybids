d522e1eb46f11fae0ca1591b7d972d0074229e89
thikimyen-nguyen <nguyenthikimyen25@gmail.com>
2023-12-07 13:42:56 +0100

    fix listing UX with logged user

--- src/js/profile/media/update.mjs
+++ src/js/profile/media/update.mjs
@@ -1,33 +1,31 @@
 import { userToken } from "../../auth_API/token.mjs";
 import { profileURL } from "../../auth_API/api.mjs";
-
+import { message } from "../../data/message.mjs";
 export async function updateAvatar(url, data) {
- try {
-  const putDataOption = {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${userToken}`,
-    },
-    body: JSON.stringify(data),
-  };
+  try {
+    const putDataOption = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
+      body: JSON.stringify(data),
+    };
 
-  const response = await fetch(url, putDataOption);
-  const json = await response.json();
-  if (response.ok) {
-    alert("Your avatar is updated!");
-    window.location.reload();
-    return json;
-    
-  } else {
-    throw new Error("Could not update avatar!")
+    const response = await fetch(url, putDataOption);
+    const json = await response.json();
+    if (response.ok) {
+      alert("Your avatar is updated!");
+      window.location.reload();
+      return json;
+    } else {
+      throw new Error("Could not update avatar!");
+    }
+  } catch (error) {
+    const loader = document.querySelector(".mediaNote");
+    loader.classList.add("text-secondary", "bg-light");
+    loader.innerHTML = message("error", error);
   }
- } catch (error) {
-  const loader = document.querySelector(".mediaNote");
-  loader.classList.add("text-secondary", "bg-light");
-  loader.innerHTML = message("error", error);
- }
-  
 }
 
 const mediaForm = document.querySelector("#updateAvatarForm");
@@ -38,14 +36,13 @@ const updateMediaURL = profileURL + `/${currentUser}/media`;
 export function updateMedia() {
   mediaForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    
+
     const newAvatar = {
-      "avatar": avatarInput.value,
+      avatar: avatarInput.value,
     };
-   
-      updateAvatar(updateMediaURL, newAvatar);
- 
-  })
+
+    updateAvatar(updateMediaURL, newAvatar);
+  });
 }
 
 // get new update user profile to show new media
